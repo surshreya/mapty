eccd1ba627961426320e80b6eeea6920593e6ff9
surshreya <surshreya.0727@gmail.com>
2022-12-26 11:51:19 +0530

    initial map function added

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    var map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
+  },
+  function () {
+    alert("Could not get your coordinate!");
+  }
+);
